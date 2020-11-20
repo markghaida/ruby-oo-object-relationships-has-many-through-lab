3daa4d260b5de8d2335f6aebffd973188ecbdfaf
Mark Ghaida <markghaida@gmail.com>
2020-11-19 23:51:54 -0500

    completed this lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
 
+class Song 
+    attr_accessor :name, :artist, :genre  
+    @@all = []
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre 
+        @@all << self
+        # binding.pry  
+    end 
+
+    def self.all 
+        @@all
+        #binding.pry
+    end 
+    
+end 
